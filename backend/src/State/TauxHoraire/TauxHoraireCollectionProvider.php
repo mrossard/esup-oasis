85d9dd46015d4868dedf633dcc4b59b528dc4591
Manuel Rossard <manuel.rossard@u-bordeaux.fr>
2024-12-03 15:50:33 +0100

    feat(back): ajout GetCollection sur taux horaires

---
+++ backend/src/State/TauxHoraire/TauxHoraireCollectionProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\State\TauxHoraire;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\TauxHoraire;
+use App\State\TransformerService;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class TauxHoraireCollectionProvider implements ProviderInterface
+{
+    public function __construct(
+        #[Autowire(service: 'api_platform.doctrine.orm.state.collection_provider')] private readonly ProviderInterface $collectionProvider,
+        private readonly TransformerService                                                                            $transformerService
+    )
+    {
+
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        $data = $this->collectionProvider->provide($operation, $uriVariables, $context);
+
+        return array_map(fn($taux) => $this->transformerService->transform($taux, TauxHoraire::class), iterator_to_array($data));
+    }
+}
