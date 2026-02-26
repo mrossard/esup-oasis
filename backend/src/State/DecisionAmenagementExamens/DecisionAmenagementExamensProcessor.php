771ebfa2ed0c425632544de55d498b9388a26f69
mrossard <manuel.rossard@u-bordeaux.fr>
2026-02-26 08:57:35 +0100

    fix: invalidation /utilisateurs/{uid} sur patch décision

--- backend/src/State/DecisionAmenagementExamens/DecisionAmenagementExamensProcessor.php
+++ backend/src/State/DecisionAmenagementExamens/DecisionAmenagementExamensProcessor.php
@@ -50,6 +50,7 @@ readonly class DecisionAmenagementExamensProcessor implements ProcessorInterface
             assert($user instanceof Utilisateur);
 
             $this->messageBus->dispatch(new DecisionEditionDemandeeMessage($entity->getId(), $user->getUid()));
+            $this->messageBus->dispatch(new RessourceModifieeMessage(new \App\ApiResource\Utilisateur($entity->getBeneficiaire())));
         }
 
         return $data;
