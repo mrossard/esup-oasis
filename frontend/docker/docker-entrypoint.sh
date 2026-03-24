8fc7fbe654e00fe69550760a453a48eceb2618f0
jlemon100p <julien.lemonnier@u-bordeaux.fr>
2026-03-24 15:05:50 +0100

    fix(front): update dependencies

--- frontend/docker/docker-entrypoint.sh
+++ frontend/docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Injecter les variables d'environnement dans le fichier env.js
-npx react-inject-env set --dir /usr/share/nginx/html
+npx --loglevel error react-inject-env set --dir /usr/share/nginx/html
 
 # Exécuter Nginx
 exec nginx -g "daemon off;"
