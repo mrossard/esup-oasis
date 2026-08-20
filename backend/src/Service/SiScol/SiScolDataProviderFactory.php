9c91754800aca3e37041c6743c3c208dbe1ce963
Kevin Leveillard <leveillard.kevin@hotmail.com>
2026-08-20 15:49:23 +0200

    [feat] Correction factory pour les SI de scolarité
    - Ajout AutowireTag sur la classe abstraite
    - Ajout d'une fonction getProviderId()
    - Ajout de la fonction getProviderId() dans la classse FakeSiScolDataProvider
    - Modification de la factory pour retourner le bon provider avec son constructeur (s'il en existe un)
    - Ajout tests avec la classe FakeSiScolProviderFactoryTest

--- backend/src/Service/SiScol/SiScolDataProviderFactory.php
+++ backend/src/Service/SiScol/SiScolDataProviderFactory.php
@@ -5,6 +5,7 @@
  * This file is part of the Esup-Oasis project (https://github.com/EsupPortail/esup-oasis).
  *  For full copyright and license information please view the LICENSE file distributed with the source code.
  *
+ *  @author Manuel Rossard <manuel.rossard@u-bordeaux.fr>
  *  @author Kevin Leveillard <kevin.leveillard@agroparistech.fr>
  *
  */
@@ -14,6 +15,7 @@ namespace App\Service\SiScol;
 use InvalidArgumentException;
 use LogicException;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 
 class SiScolDataProviderFactory
 {
@@ -21,36 +23,27 @@ class SiScolDataProviderFactory
     public function __construct(
         #[Autowire(env: 'resolve:SI_SCOL')]
         private string $siScol,
+        #[AutowireIterator('oasis.siscol_provider')]
+        private iterable $providers
     ) {}
 
     /**
      * Création du provider en fonction de la variable d'environnement SI_SCOL
      *
-     * @author leveillard
+     * @return string
      */
     public function create(): AbstractSiScolDataProvider
     {
-        $className = sprintf(
-            'App\\Service\\Serialize\\%sProvider',
-            ucfirst(strtolower($this->siScol))
+        $providerName = ucfirst(strtolower($this->siScol));
+
+        foreach ($this->providers as $provider) {
+            if (strcasecmp($provider->getProviderId(), $this->siScol) === 0) {
+                return $provider;
+            }
+        }
+
+        throw new InvalidArgumentException(
+            sprintf('Le provider "%s" n\'existe pas.', $this->siScol)
         );
-
-        if (!class_exists($className)) {
-            throw new InvalidArgumentException(
-                sprintf('Le provider "%s" n\'existe pas.', $this->siScol)
-            );
-        }
-
-        $provider = new $className();
-
-        if (!$provider instanceof AbstractSiScolDataProvider) {
-            throw new LogicException(
-                sprintf(
-                    'La classe "%s" doit hériter de AbstractSiScolDataProvider.',
-                    $className
-                )
-            );
-        }
-        return $provider;
     }
 }
