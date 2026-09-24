ec6567e7bb3c8aaf4c49be67aa8789884899c80b
mrossard <manuel.rossard@u-bordeaux.fr>
2026-09-24 09:19:58 +0200

    fix: erreur de copier/coller

--- backend/migrations/Version20260924085200.php
+++ backend/migrations/Version20260924085200.php
@@ -12,7 +12,7 @@ final class Version20260924085200 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Ajout paramètre LIEU_COURRIER';
+        return 'Ajout paramètre ADRESSE_POSTALE';
     }
 
     public function up(Schema $schema): void
